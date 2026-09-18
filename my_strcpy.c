25cf04fb41b0518d2c2fd045998c34b8013b88a2
giaapen <giacomopernice@yahoo.fr>
2026-09-18 15:16:10 +0200

    correction my_strcpy.c

--- my_strcpy.c
+++ my_strcpy.c
@@ -1,17 +1,15 @@
- #include "my_libc.h"
- 
+#include "my_libc.h"
 
-
-char *my_strcpy(char *dst, const  char *src)
+char *
+my_strcpy(char *dst, const char *src)
 {
-        int i;
+	int i;
 
-        i = 0;
-        while (src[i] != '\0')
-        {
-                dst[i] = src[i];
-                i++;
-        }
-        dst[i] = '\0';
-        return (dst);
-} 
+	i = 0;
+	while (src[i] != '\0') {
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
